3563138071ee265632929e4aca18095124e0bda1
Damian Mee <spam@meedamian.com>
2020-01-25 16:08:21 +0700

    protos only

--- gen-protos.sh
+++ gen-protos.sh
@@ -1,35 +1,76 @@
 #!/bin/sh
 
+TAG=$1
+
 REPO=lightningnetwork/lnd
 
-TAGS=$(curl -s "https://api.github.com/repos/${REPO}/tags" | jq -r '.[].name' | sed 's|-beta||' | grep '^v\d.\d.\d$' | tac)
+fetch_all_protos() {
+  tag=$1
+  dir=$2
 
-DIR=lnd/google/api
-mkdir -p "${DIR}"
-curl -s https://raw.githubusercontent.com/googleapis/googleapis/master/google/api/annotations.proto > "${DIR}/annotations.proto"
-curl -s https://raw.githubusercontent.com/googleapis/googleapis/master/google/api/http.proto > "${DIR}/http.proto"
+  if [ "${dir}" != "" ]; then
+    if [ "${dir}" != "${dir#https}" ]; then
+      echo "${dir}"
+      return
+    fi
+
+      dir="/${dir}"
+  fi
+
+  list=$(curl -sL "https://api.github.com/repos/${REPO}/contents/lnrpc${dir}?ref=${tag}" \
+    | jq -r '.[] | if (.name | endswith("proto")) then .download_url elif (.type == "dir") then .name else empty end')
+
+  for l in ${list}; do
+    fetch_all_protos "${tag}" "${l}"
+  done
+}
 
 
-prev=""
-for t in ${TAGS}; do
-	echo "Processing ${t}…"
+download() {
+  # Download all dependencies external to lnd
+  DIR=google/api
+  mkdir -p "${DIR}"
+  curl -s https://raw.githubusercontent.com/googleapis/googleapis/master/google/api/annotations.proto > "${DIR}/annotations.proto"
+  curl -s https://raw.githubusercontent.com/googleapis/googleapis/master/google/api/http.proto > "${DIR}/http.proto"
 
-	mkdir -p "${t}"
+  TAGS=$(curl -s "https://api.github.com/repos/${REPO}/tags" | jq -r '.[].name' | sed 's|-beta||' | grep '^v\d.\d.\d$' | tac)
 
-	proto_file=rpc.proto
-	go_file=rpc.pb.go
-	curl -s "https://raw.githubusercontent.com/${REPO}/${t}-beta/lnrpc/${proto_file}" > "${t}/${proto_file}"
+  for t in ${TAGS}; do
+    if [ "${TAG}" != "" ] && [ "${TAG}" != "${t}" ]; then
+      continue
+    fi
 
-	cat "${t}/${proto_file}" | sed 's|github.com/lightningnetwork/lnd/lnrpc|lnd|'  > "lnd/${proto_file}"
+    echo "Processing ${t}…"
 
-	protoc --go_out=plugins=grpc,paths=source_relative:./lnd/  -Ilnd/  "lnd/${proto_file}"
+    mkdir -p "${t}"
 
-	mv "lnd/${go_file}"  "${t}/"
+    for proto in $(fetch_all_protos "${t}-beta"); do
+      printf "Downloading: %s…\n" "${proto}"
+      wget -nH --cut-dirs=4  -xqcP "${t}/"  "${proto}"
+    done
 
-	if [[ -n "${prev}" ]]; then
-		diff  "${prev}/${proto_file}"	"${t}/${proto_file}"	> "${t}/proto-diff-to-${prev}.patch"
-		diff  "${prev}/${go_file}"		"${t}/${go_file}"			> "${t}/go-diff-to-${prev}.patch"
-	fi
+    echo
+  done
+}
 
-	prev=${t}
-done
+generate() {
+
+
+ls
+
+  #	go_file=rpc.pb.go
+
+  #	cat "${t}/${proto_file}" | sed 's|github.com/lightningnetwork/lnd/lnrpc|lnd|'  > "lnd/${proto_file}"
+  #
+  #	protoc --go_out=plugins=grpc,paths=source_relative:./lnd/  -Ilnd/  "lnd/${proto_file}"
+  #
+  #	mv "lnd/${go_file}"  "${t}/"
+  #
+  #	if [[ -n "${prev}" ]]; then
+  #		diff  "${prev}/${proto_file}"	"${t}/${proto_file}"	> "${t}/proto-diff-to-${prev}.patch"
+  #		diff  "${prev}/${go_file}"		"${t}/${go_file}"			> "${t}/go-diff-to-${prev}.patch"
+  #	fi
+  #
+
+
+}
